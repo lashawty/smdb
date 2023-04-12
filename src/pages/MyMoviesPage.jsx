3eb126bc01c23d36cd55bdce854fedf7edf73b48
lashawty <hustle0985@gmail.com>
2023-04-12 14:09:02 +0800

    0412

--- src/pages/MyMoviesPage.jsx
+++ src/pages/MyMoviesPage.jsx
@@ -4,21 +4,24 @@ import axios from 'axios';
 export default function MyMoviesPage(props) {
 
   //redux
-  const [id, setID] = useState(null)
   const [myMovies, setMyMovies] = useState([])
 
   const sessionId = useSelector(state => state.session.value);
+  const isLogin = useSelector(state => state.login.value);
 
-  useEffect(()=>{
-    axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/account/%7Baccount_id%7D/favorite/movies?api_key=06b5ea731fca9e39d8b51074aaad5aac&session_id=${sessionId}&language=en-US&sort_by=created_at.asc&page=1`)
-      .then(response => {
-        setMyMovies(response.data.results)
-        console.log(myMovies);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },[])
+  if(sessionId && isLogin) {
+    useEffect(()=>{
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/account/%7Baccount_id%7D/favorite/movies?api_key=06b5ea731fca9e39d8b51074aaad5aac&session_id=${sessionId}&language=en-US&sort_by=created_at.asc&page=1`)
+        .then(response => {
+          setMyMovies(response.data.results)
+          console.log(myMovies);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },[isLogin])
+  }
+  
 
   return (
     <>
